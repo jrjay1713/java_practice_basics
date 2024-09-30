2e8f9d78f944c04d15afa20b2cc6f9e49c70d506
jrjay1713 <ajaych1713@gmail.com>
2024-09-30 14:06:10 +0530

    Add files via upload

---
+++ varibles.java
@@ -0,0 +1,19 @@
+public class varibles {
+    public static void main(String[] args){
+
+        int number = 51;
+        double decimals = 10.1;
+        boolean zero1 = true;
+        char myChar = 'A';
+        String myText = "StartHere";
+
+        // printing variables
+
+        System.out.println("number = " +number);
+        System.out.println("decimal = " +decimals);
+        System.out.println("boolean = " +zero1);
+        System.out.println("Character" + myChar);
+        System.out.println("string = " +myText);
+
+    }    
+}
